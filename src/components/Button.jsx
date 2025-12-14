4498381f8b84bd63b6e0f36b1c990194fd0e1b33
Kiko Johnny <josuekiko3@gmail.com>
2025-12-14 22:20:51 +0100

    dernier modification

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@
 export default function Button({color,children,fonction}) {
   return (
     
-    <button onClick={fonction} className={`font-monserrat inline-flex items-center gap-2 text-[14px]  self-start ${color}  px-3 py-2 rounded-lg text-white  cursor-pointer `}> {children} </button>
+    <button onClick={fonction} className={`font-monserrat inline-flex items-center gap-2 text-[14px]  self-start ${color} font-bold  px-3.5 py-2 rounded-2xl text-white  cursor-pointer `}> {children} </button>
     
   )
 }
